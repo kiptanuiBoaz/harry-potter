c458053d043534e422fc8533029394469218765e
Boaz <boaserem022@gmail.com>
2023-08-18 12:33:44 +0300

    added AuthState in authslice

--- lib/getDefaultTheme.ts
+++ lib/getDefaultTheme.ts
@@ -1,8 +1,12 @@
 "use client"
 export const getDefautlTheme = () => {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (typeof window !== 'undefined') {
+       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         return 'dark';
     } else {
         return 'light';
     }
+      }
+      
+    
 }
